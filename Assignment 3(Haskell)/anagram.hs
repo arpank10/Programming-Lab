da82d26b5fc0324348215e69958f14cdc8a4525e
Arpit Gupta <alphaWizard@users.noreply.github.com>
2019-11-20 19:29:39 +0530

    added comments

--- Assignment 3(Haskell)/anagram.hs
+++ Assignment 3(Haskell)/anagram.hs
@@ -1,32 +1,32 @@
-import Data.List
+import Data.List  -- For doing operations on list
 
-check_anagram a b = (sort a == sort b)
+check_anagram a b = (sort a == sort b)  -- Anagrams have equal sorted representation
 
 find_anagram sub n= do 
- i <- [0..n-2]
+ i <- [0..n-2]  -- -- compare i'th substring
  let res = []
  (do 
-   j <- [i+1..n-1]
+   j <- [i+1..n-1]  -- with j'th substring
    let s1 = sub!!i 
    let s2 = sub!!j
-   if check_anagram s1 s2
+   if check_anagram s1 s2  -- if anagrams, append (i,j) pair to the list of answers
    then [(s1,s2)]:res
    else res)
  
  
 
 getSub str n = do 
- i <- [1..n]
- let res = []
- let k = n - i
+ i <- [1..n]  -- iterate over lengths from 1 to n
+ let res = [] -- store all substrings
+ let k = n - i  -- no. of drops to be made for length i
  (do 
-   j <- [0..k]
+   j <- [0..k]  -- drop j items from front each time and take i items(gives all i length substring)
    take i(drop j str):res)
 
-anagram [list1, list2] = do
- let str = list1 ++ list2
- let len = length str
- let subs = getSub str len
- let anagrams = find_anagram subs (length subs)
+anagram [list1, list2] = do 
+ let str = list1 ++ list2  -- append the two lists inputed for simplicity
+ let len = length str  -- length of the string after concatenation
+ let subs = getSub str len  -- find all substrings 
+ let anagrams = find_anagram subs (length subs) -- find all pairs of anagrams among the substrings 
  print(length anagrams)
  print(anagrams)
